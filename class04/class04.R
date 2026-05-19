714bfccf4619a3e0ea4146fb6e97fe51a7a2a5c1
gisellegarcia$ <gisellegarcia@Giselles-MacBook-Air-4.local>
2026-05-19 12:55:40 -0700

    Add class 4 to 7

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Classs 4: Intro to R"
+#' author: "Giselle Garcia A17995734"
+#' date: "April 9th, 2026"
+#' ---
+
+# this is just a text file
+x <- 1:50
+plot(x,sin(x), type="l", col="green", lwd=3)
